d70994192068d00aa3d80c9b120b7284f1d6e69f
Sagarpatidar0969 <sppatidar27@gmail.com>
2023-10-03 22:57:36 +0530

    done

--- Corejava/src/Examples/SecondLargest.java
+++ Corejava/src/Examples/SecondLargest.java
@@ -1,5 +1,25 @@
 package Examples;
 
+//public class SecondLargest {
+//	public static void main(String[] args) {
+//		
+//		 
+//		int arr[] = {1,6,8,7,4,5};
+//		for (int i = 0; i < arr.length; i++) {
+//			
+//			for (int j = i+1; j < arr.length; j++) {
+//				
+//				if(arr[i]>arr[j]) {
+//					int temp = arr[i];
+//					arr[i] = arr[j];
+//					arr[j] = temp;
+//				}
+//			}
+//		}
+//		System.out.println(arr[arr.length - 2]);
+//	}
+//
+//}
 public class SecondLargest {
 	public static void main(String[] args) {
 		
@@ -10,7 +30,7 @@ public class SecondLargest {
 			for (int j = i+1; j < arr.length; j++) {
 				
 				if(arr[i]>arr[j]) {
-					int temp = arr[i];
+					int temp = arr[i]; 
 					arr[i] = arr[j];
 					arr[j] = temp;
 				}
